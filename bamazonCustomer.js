bcc43a80fdf3bf86aa9fda1f1c6b87c78b283a2b
Manasi <manudee@gmail.com>
2017-11-13 23:33:56 -0500

    customer view complete

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,7 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
+
 
 var connection = mysql.createConnection({
 	host: "localhost",
@@ -21,16 +23,18 @@ function start(){
 	connection.query(sql, function(err, results) {
 		if (err) throw err;
 		console.log("Item_id|product_name|price");
+
 		for (var i = 0; i < results.length; i++) {
 			console.log(results[i].item_id + "------|----" + results[i].product_name  + "-----|" + results[i].price);
 		}
-
+		userInput();
 	})
+}
 
 
-
+function userInput(){
 	inquirer.prompt([{
-		name: "productID",
+		name: "item_id",
 		type: "input",
 		message: "What product ID you want to buy?"
 	},
@@ -39,8 +43,54 @@ function start(){
 		type: "input",
 		message: "How many units you want to buy?"
 	}]).then(function(answer){
-		console.log(answer.productID);
+		console.log(answer.item_id);
 		console.log(answer.quantity);
+
+
+
+		var sql = `select stock_quantity from products where item_id = ${answer.item_id}`;
+		connection.query(sql, function(err,rows){
+			console.log(rows[0].stock_quantity);
+			var stock_quantity = rows[0].stock_quantity;
+
+			if(stock_quantity < answer.quantity)
+				console.log("Insufficient quantity!");
+
+			else
+				updateDB(answer.item_id, answer.quantity);
+
+
+		})
+
 	})
+
+}
+
+
+function updateDB(item_id,quantity){
+
+	var sql = `update products set stock_quantity = stock_quantity - ${quantity} where item_id = ${item_id}`;
+
+	connection.query(sql,function(err,rows){
+		console.log(rows.affectedRows + " bamazon updated");
+		
+		var select = `SELECT item_id,product_name,price,stock_quantity FROM products where item_id = ${item_id}`;
+		
+		connection.query(select, function(err, results) {
+			if (err) throw err;
+			console.log("Item_id|product_name|price");
+			console.log(results[0].item_id + "------|----" + results[0].product_name  + "-----|" + results[0].price + "-----|" + results[0].stock_quantity);
+			
+			var product = results[0].product_name;
+			var cost = results[0].price * quantity;
+			console.log("Your total cost for " + product + " is $ " + parseFloat(cost,2));
+		})
+
+
+		connection.end();
+	
+	});
+
+	
 
 }
